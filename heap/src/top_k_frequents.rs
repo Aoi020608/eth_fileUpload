66c148ea6b5ea73ca533863cdfa26b06d265388d
Aoi <ht.so.ik0102@gmail.com>
2022-04-28 15:55:30 +0700

    top_k_frequent_2

--- heap/src/top_k_frequents.rs
+++ heap/src/top_k_frequents.rs
@@ -30,6 +30,25 @@ pub fn top_k_frequent_1(nums: Vec<i32>, k: i32) -> Vec<i32> {
     res
 }
 
+pub fn top_k_frequent_2(nums: Vec<i32>, k: i32) -> Vec<i32> {
+    let mut res = Vec::with_capacity(k as usize);
+    let mut occurence_map = HashMap::new();
+    for num in &nums {
+        *occurence_map.entry(num).or_insert(0) += 1;
+    }
+    let mut heap = BinaryHeap::new();
+    for (num, o) in occurence_map {
+        heap.push((o, num));
+    }
+
+    for _ in 0..k {
+        let (_, num) = heap.pop().unwrap();
+        res.push(*num);
+    }
+
+    res
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
