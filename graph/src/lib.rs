2b31a2dee301ba37f085408d89e9c7f94ef7850a
Aoi <ht.so.ik0102@gmail.com>
2022-04-29 21:48:18 +0700

    Create repository #17

--- graph/src/lib.rs
+++ graph/src/lib.rs
@@ -1,2 +1,3 @@
 // mod number_of_islands;
-mod max_area_of_idland;
+// mod max_area_of_idland;
+mod merge_two_binary_trees;
---
+++ graph/src/merge_two_binary_trees.rs
@@ -0,0 +1,48 @@
+/*
+#17
+You are given two binary trees root1 and root2.
+
+Imagine that when you put one of them to cover the other, some nodes of the two trees are overlapped while the others are not.
+You need to merge the two trees into a new binary tree. The merge rule is that if two nodes overlap, then sum node values up as the new value of the merged node. Otherwise, the NOT null node will be used as the node of the new tree.
+
+Return the merged tree.
+
+Note: The merging process must start from the root nodes of both trees.
+
+Input: root1 = [1,3,2,5], root2 = [2,1,3,null,4,null,7]
+Output: [3,4,5,5,4,null,7]
+
+*/
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+use std::cell::RefCell;
+use std::rc::Rc;
+impl Solution {
+    pub fn merge_trees(
+        root1: Option<Rc<RefCell<TreeNode>>>,
+        root2: Option<Rc<RefCell<TreeNode>>>,
+    ) -> Option<Rc<RefCell<TreeNode>>> {
+
+        
+
+
+
+
+    }
+}
