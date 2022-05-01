fe573acc9913bdaa6b2f3fd25ca041979ff928a6
Aoi <ht.so.ik0102@gmail.com>
2022-05-02 06:40:32 +0700

    #31

--- dynamic-programming/src/best_time_to_buy_and_sell_stock.rs
+++ dynamic-programming/src/best_time_to_buy_and_sell_stock.rs
@@ -1,4 +1,5 @@
 /*
+#31
 You are given an array prices where prices[i] is the price of a given stock on the ith day.
 
 You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
@@ -24,7 +25,7 @@ impl Solution {
             if prices[0] < 0 {
                 return 0;
             } else {
-                return prices[0];
+                return 0;
             }
         }
 
@@ -44,18 +45,45 @@ impl Solution {
 
         ans
     }
+
+    pub fn max_profit_1(prices: Vec<i32>) -> i32 {
+        let len = prices.len();
+        let mut ans = prices[0];
+
+        if len <= 1 {
+            if prices[0] < 0 {
+                return 0;
+            } else {
+                return 0;
+            }
+        }
+
+        prices.iter().fold(0, |max_prices, price| {
+            ans = ans.min(*price);
+            max_prices.max(price - ans)
+        })
+    }
 }
 
 #[test]
 fn test_max_profit() {
     let prices = vec![7, 1, 5, 3, 6, 4];
-    let ans = Solution::max_profit(prices);
+    let ans = Solution::max_profit_1(prices);
     assert_eq!(ans, 5);
 }
 
 #[test]
 fn test_max_profit_2() {
     let prices = vec![7, 6, 4, 3, 1];
-    let ans = Solution::max_profit(prices);
+    let ans = Solution::max_profit_1(prices);
     assert_eq!(ans, 0);
+
+    let a = [1, 2, 3];
+
+    // the sum of all of the elements of the array
+    let sum = a.iter().fold(0, |acc, x| acc + x);
+
+    assert_eq!(sum, 6);
 }
+
+// fold
