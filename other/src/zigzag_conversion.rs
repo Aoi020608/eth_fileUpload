013c5f3635c380bc02f9d0e49c36aeea788309c9
Aoi <ht.so.ik0102@gmail.com>
2022-05-04 12:50:17 +0700

    #54

--- other/src/zigzag_conversion.rs
+++ other/src/zigzag_conversion.rs
@@ -8,9 +8,28 @@ struct Solution;
 
 impl Solution {
     pub fn convert(s: String, num_rows: i32) -> String {
-        
+        let num_rows: usize = num_rows as usize;
+        if num_rows == 1 || num_rows > s.len() {
+            return s;
+        }
 
-        "".to_string()
+        let mut zigzag = vec![vec![]; num_rows];
+        let mut row = 0;
+        let mut step = 1;
+
+        for c in s.chars() {
+            zigzag[row].push(c);
+
+            if row == 0 {
+                step = 1;
+            } else if row == num_rows - 1 {
+                step = -1;
+            }
+
+            row = (row as isize + step) as usize;
+        }
+
+        return zigzag.iter().fold("".to_string(), |s, v| format!("{}{}", s, v.iter().collect::<String>()));
     }
 }
 
@@ -19,8 +38,18 @@ impl Solution {
 mod tests {
     use super::*;
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn test_convert() {
+        let s = "PAYPALISHIRING".to_string();
+        let num_rows = 3;
+        let result = Solution::convert(s, num_rows);
+        assert_eq!(result, "PAHNAPLSIIGYIR".to_string());
+    }
+
+    #[test]
+    fn test_convert_01() {
+        let s = "PAYPALISHIRING".to_string();
+        let num_rows = 4;
+        let result = Solution::convert(s, num_rows);
+        assert_eq!(result, "PINALSIGYAHRPI".to_string());
     }
 }
