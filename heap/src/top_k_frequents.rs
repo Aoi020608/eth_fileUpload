434b87ce03f06e454dfede0ec1d25e187bdc1d75
Aoi <ht.so.ik0102@gmail.com>
2022-05-05 15:26:58 +0700

    #5

--- heap/src/top_k_frequents.rs
+++ heap/src/top_k_frequents.rs
@@ -5,48 +5,68 @@
 // Output: [1,2]
 use std::collections::*;
 
-pub fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
-    let frequent_elements: Vec<i32> = Vec::new();
+struct Solution;
 
-    frequent_elements
-}
-
-pub fn top_k_frequent_1(nums: Vec<i32>, k: i32) -> Vec<i32> {
-    let mut res = Vec::with_capacity(k as usize);
-    let mut occurence_map = HashMap::new();
-    for num in &nums {
-        *occurence_map.entry(num).or_insert(0) += 1;
-    }
-    let mut heap = BinaryHeap::new();
-    for (num, o) in occurence_map {
-        heap.push((o, num));
+impl Solution {
+    #[allow(dead_code)]
+    pub fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
+        let frequent_elements: Vec<i32> = Vec::new();
+        frequent_elements
     }
 
-    for _ in 0..k {
-        let (_, num) = heap.pop().unwrap();
-        res.push(*num);
+    #[allow(dead_code)]
+    pub fn top_k_frequent_1(nums: Vec<i32>, k: i32) -> Vec<i32> {
+        let mut res = Vec::with_capacity(k as usize);
+        let mut occurence_map = HashMap::new();
+        for num in &nums {
+            *occurence_map.entry(num).or_insert(0) += 1;
+        }
+        let mut heap = BinaryHeap::new();
+        for (num, o) in occurence_map {
+            heap.push((o, num));
+        }
+        for _ in 0..k {
+            let (_, num) = heap.pop().unwrap();
+            res.push(*num);
+        }
+        res
     }
 
-    res
-}
-
-pub fn top_k_frequent_2(nums: Vec<i32>, k: i32) -> Vec<i32> {
-    let mut res = Vec::with_capacity(k as usize);
-    let mut occurence_map = HashMap::new();
-    for num in &nums {
-        *occurence_map.entry(num).or_insert(0) += 1;
+    #[allow(dead_code)]
+    pub fn top_k_frequent_2(nums: Vec<i32>, k: i32) -> Vec<i32> {
+        let mut res = Vec::with_capacity(k as usize);
+        let mut occurence_map = HashMap::new();
+        for num in &nums {
+            *occurence_map.entry(num).or_insert(0) += 1;
+        }
+        let mut heap = BinaryHeap::new();
+        for (num, o) in occurence_map {
+            heap.push((o, num));
+        }
+        for _ in 0..k {
+            let (_, num) = heap.pop().unwrap();
+            res.push(*num);
+        }
+        res
     }
-    let mut heap = BinaryHeap::new();
-    for (num, o) in occurence_map {
-        heap.push((o, num));
-    }
 
-    for _ in 0..k {
-        let (_, num) = heap.pop().unwrap();
-        res.push(*num);
+    #[allow(dead_code)]
+    pub fn top_k_frequent_03(nums: Vec<i32>, k: i32) -> Vec<i32> {
+        let mut ret: Vec<i32> = Vec::with_capacity(k as usize);
+        let mut map: HashMap<i32, i32> = HashMap::new();
+        for num in nums.iter() {
+            *map.entry(*num).or_insert(0) += 1;
+        }
+        let mut heap = BinaryHeap::new();
+        for (num, o) in map.iter() {
+            heap.push((o, num));
+        }
+        for _ in 0..k {
+            let (_, num) = heap.pop().unwrap();
+            ret.push(*num);
+        }
+        ret
     }
-
-    res
 }
 
 #[cfg(test)]
@@ -57,7 +77,7 @@ mod tests {
         let nums = vec![1, 1, 1, 2, 2, 3];
         let k = 1;
 
-        let ans = top_k_frequent_1(nums, k);
+        let ans = Solution::top_k_frequent_03(nums, k);
         assert_eq!(ans, [1]);
     }
 
@@ -66,7 +86,7 @@ mod tests {
         let nums = vec![1, 1, 1, 2, 2, 3, 4, 4, 4];
         let k = 2;
 
-        let ans = top_k_frequent_1(nums, k);
+        let ans = Solution::top_k_frequent_03(nums, k);
         assert_eq!(ans, [4, 1]);
     }
 }
