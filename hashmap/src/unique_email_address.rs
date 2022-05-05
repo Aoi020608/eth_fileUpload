a68c44e95d99bd62039d1e0f05a709ccbfbb3cf8
Aoi <ht.so.ik0102@gmail.com>
2022-05-05 20:26:57 +0700

    Update #9

--- hashmap/src/unique_email_address.rs
+++ hashmap/src/unique_email_address.rs
@@ -1,36 +1,70 @@
-// Every valid email consists of a local name and a domain name,
-// separated by the '@' sign. Besides lowercase letters, the email may contain one or more '.' or '+'.
+// #9
+
 use std::collections::HashMap;
 
-pub fn num_unique_emails(emails: Vec<String>) -> i32 {
-    let mut map: HashMap<String, i32> = HashMap::new();
-    for email in emails.into_iter() {
-        let trimmed = email.trim().split('@').collect::<Vec<&str>>();
-        let (local, domain) = (trimmed[0], trimmed[1]);
-        let local = local
-            .chars()
-            .filter(|&x| x != '.')
-            .collect::<Vec<char>>()
-            .into_iter()
-            .collect::<String>();
+struct Solution;
 
-        if local.contains('+') {
-            let local = *local.split('+').collect::<Vec<&str>>().first().unwrap();
-            let key = local.to_string() + "@" + domain;
-            if !map.contains_key(&key) {
-                let count = map.entry(key).or_insert(0);
-                *count += 1;
-            }
-        } else {
-            let key = local + "@" + domain;
-            if !map.contains_key(&key) {
-                let count = map.entry(key).or_insert(0);
-                *count += 1;
+impl Solution {
+    #[allow(dead_code)]
+    pub fn num_unique_emails(emails: Vec<String>) -> i32 {
+        let mut map: HashMap<String, i32> = HashMap::new();
+        for email in emails.into_iter() {
+            let trimmed = email.trim().split('@').collect::<Vec<&str>>();
+            let (local, domain) = (trimmed[0], trimmed[1]);
+            let local = local
+                .chars()
+                .filter(|&x| x != '.')
+                .collect::<Vec<char>>()
+                .into_iter()
+                .collect::<String>();
+            if local.contains('+') {
+                let local = *local.split('+').collect::<Vec<&str>>().first().unwrap();
+                let key = local.to_string() + "@" + domain;
+                if !map.contains_key(&key) {
+                    let count = map.entry(key).or_insert(0);
+                    *count += 1;
+                }
+            } else {
+                let key = local + "@" + domain;
+                if !map.contains_key(&key) {
+                    let count = map.entry(key).or_insert(0);
+                    *count += 1;
+                }
             }
         }
+        map.len() as i32
     }
 
-    map.len() as i32
+    #[allow(dead_code)]
+    pub fn num_unique_emails_01(emails: Vec<String>) -> i32 {
+        let mut map = HashMap::new();
+
+        for email in emails.into_iter() {
+            let trimmed = email.trim().split('@').collect::<Vec<&str>>();
+            let (local, domain) = (trimmed[0], trimmed[1]);
+
+            let local = local
+                .chars()
+                .filter(|&x| x != '.')
+                .collect::<Vec<char>>()
+                .into_iter()
+                .collect::<String>();
+            if local.contains('+') {
+                let local = *local.split('+').collect::<Vec<&str>>().first().unwrap();
+                let key = local.to_string() + "@" + domain;
+                if !map.contains_key(&key) {
+                    *map.entry(key).or_insert(0) += 1;
+                }
+            } else {
+                let key = local.to_string() + "@" + domain;
+                if !map.contains_key(&key) {
+                    *map.entry(key).or_insert(0) += 1;
+                }
+            }
+        }
+
+        map.len() as i32
+    }
 }
 
 #[cfg(test)]
@@ -43,7 +77,7 @@ mod tests {
             "test.e.mail+bob.cathy@leetcode.com".to_string(),
             "testemail+david@lee.tcode.com".to_string(),
         ];
-        let ans = num_unique_emails(emails);
+        let ans = Solution::num_unique_emails_01(emails);
         assert_eq!(ans, 2);
     }
 
@@ -54,7 +88,7 @@ mod tests {
             "b@leetcode.com".to_string(),
             "c@leetcode.com".to_string(),
         ];
-        let ans = num_unique_emails(emails);
+        let ans = Solution::num_unique_emails_01(emails);
         assert_eq!(ans, 3);
     }
 }
