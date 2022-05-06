4365cce3c787ce99478592aa0911c352798154c3
Aoi <ht.so.ik0102@gmail.com>
2022-05-06 16:56:56 +0700

    #26

--- dynamic-programming/src/maximum_subarray.rs
+++ dynamic-programming/src/maximum_subarray.rs
@@ -1,19 +1,11 @@
-/*
-#26
-Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+// #26
 
-A subarray is a contiguous part of an array.
-
-Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
-Output: 6
-Explanation: [4,-1,2,1] has the largest sum = 6.
-
-*/
 use std::cmp;
 
 struct Solution;
 
 impl Solution {
+    #[allow(dead_code)]
     pub fn max_sub_array(nums: Vec<i32>) -> i32 {
         let mut sum = i32::MIN;
         let mut max = 0;
@@ -28,6 +20,7 @@ impl Solution {
         }
     }
 
+    #[allow(dead_code)]
     pub fn max_sub_array_1(nums: Vec<i32>) -> i32 {
         let mut ans = nums[0];
         let len_nums = nums.len();
@@ -47,9 +40,24 @@ impl Solution {
 
         println!("Answer: {:?}", ans);
         println!("Result: {:?}", result);
-
-        // *result.iter().max().unwrap()
         ans
+    }
+
+    #[allow(dead_code)]
+    pub fn max_sub_array_02(nums: Vec<i32>) -> i32 {
+        let len_nums = nums.len();
+        let mut result = nums;
+
+        for i in 1..len_nums {
+            let curr_val = result.get(i).unwrap();
+            let prev_val = result.get(i - 1).unwrap();
+
+            if curr_val + prev_val >= *curr_val {
+                *result.get_mut(i).unwrap() = curr_val + prev_val;
+            }
+        }
+
+        *result.iter().max().unwrap()
     }
 }
 
@@ -60,7 +68,6 @@ mod tests {
     fn test_num_ways() {
         let nums = vec![-2, 1, -3, 4, -1, 2, 1, -5, 4];
         let result = Solution::max_sub_array_1(nums);
-        // println!("Result: {:?}", result);
         assert_eq!(result, 6);
     }
 
@@ -68,7 +75,6 @@ mod tests {
     fn test_num_ways_1() {
         let nums = vec![1];
         let result = Solution::max_sub_array_1(nums);
-        // println!("Result: {:?}", result);
         assert_eq!(result, 1);
     }
 
