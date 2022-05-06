a69b5ff51da857b3a69e7ff787c1b8ec3ecc8075
Aoi <ht.so.ik0102@gmail.com>
2022-05-06 18:16:22 +0700

    #31

--- dynamic-programming/src/best_time_to_buy_and_sell_stock.rs
+++ dynamic-programming/src/best_time_to_buy_and_sell_stock.rs
@@ -1,22 +1,11 @@
-/*
-#31
-You are given an array prices where prices[i] is the price of a given stock on the ith day.
-
-You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
-
-Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
-
-Input: prices = [7,1,5,3,6,4]
-Output: 5
-Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
-Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
-*/
+// #31
 
 use std::collections::BinaryHeap;
 
 struct Solution;
 
 impl Solution {
+    #[allow(dead_code)]
     pub fn max_profit(prices: Vec<i32>) -> i32 {
         let len = prices.len();
         let mut ans = 0;
@@ -46,16 +35,13 @@ impl Solution {
         ans
     }
 
+    #[allow(dead_code)]
     pub fn max_profit_1(prices: Vec<i32>) -> i32 {
         let len = prices.len();
         let mut ans = prices[0];
 
         if len <= 1 {
-            if prices[0] < 0 {
-                return 0;
-            } else {
-                return 0;
-            }
+            return 0;
         }
 
         prices.iter().fold(0, |max_prices, price| {
@@ -80,10 +66,7 @@ fn test_max_profit_2() {
 
     let a = [1, 2, 3];
 
-    // the sum of all of the elements of the array
     let sum = a.iter().fold(0, |acc, x| acc + x);
 
     assert_eq!(sum, 6);
 }
-
-// fold
--- dynamic-programming/src/house_robber_2.rs
+++ dynamic-programming/src/house_robber_2.rs
@@ -1,23 +1,11 @@
-/*
-You are a professional robber planning to rob houses along a street.
-Each house has a certain amount of money stashed. All houses at this place are arranged in a circle.
-That means the first house is the neighbor of the last one.
-Meanwhile, adjacent houses have a security system connected,
-and it will automatically contact the police if two adjacent houses were broken into on the same night.
-
-Given an integer array nums representing the amount of money of each house,
-return the maximum amount of money you can rob tonight without alerting the police.
-
-Input: nums = [2,3,2]
-Output: 3
-Explanation: You cannot rob house 1 (money = 2) and then rob house 3 (money = 2), because they are adjacent houses.
-*/
+// #30
 
 use std::cmp::max;
 
 struct Solution;
 
 impl Solution {
+    #[allow(dead_code)]
     pub fn rob(nums: Vec<i32>) -> i32 {
         let len = nums.len();
         let mut cloned_nums = nums.clone();
@@ -55,6 +43,7 @@ impl Solution {
         }
     }
 
+    #[allow(dead_code)]
     pub fn rob_1(nums: Vec<i32>) -> i32 {
         let n = nums.len();
 
@@ -68,6 +57,7 @@ impl Solution {
         Self::helper(&nums[0..n - 1]).max(Self::helper(&nums[1..n]))
     }
 
+    #[allow(dead_code)]
     fn helper(nums: &[i32]) -> i32 {
         let (mut h1, mut h2) = (0, 0);
 
@@ -82,7 +72,7 @@ impl Solution {
 
 #[test]
 fn test_rob() {
-    let mut input = vec![2, 3, 2];
+    let input = vec![2, 3, 2];
     let ans = Solution::rob(input);
 
     assert_eq!(ans, 3);
@@ -90,7 +80,7 @@ fn test_rob() {
 
 #[test]
 fn test_rob_1() {
-    let mut input = vec![1, 2, 3, 1];
+    let input = vec![1, 2, 3, 1];
     let ans = Solution::rob(input);
 
     assert_eq!(ans, 4);
@@ -98,7 +88,7 @@ fn test_rob_1() {
 
 #[test]
 fn test_rob_2() {
-    let mut input = vec![1, 2, 3];
+    let input = vec![1, 2, 3];
     let ans = Solution::rob(input);
 
     assert_eq!(ans, 3);
@@ -106,7 +96,7 @@ fn test_rob_2() {
 
 #[test]
 fn test_rob_3() {
-    let mut input = vec![1, 2, 1, 1];
+    let input = vec![1, 2, 1, 1];
     let ans = Solution::rob(input);
 
     assert_eq!(ans, 3);
