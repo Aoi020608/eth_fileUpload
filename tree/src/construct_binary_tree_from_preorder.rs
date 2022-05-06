6504f55375350b754274651f554aee2599abbb9d
Aoi <ht.so.ik0102@gmail.com>
2022-05-06 15:49:50 +0700

    construct binary tree #23

--- tree/src/construct_binary_tree_from_preorder.rs
+++ tree/src/construct_binary_tree_from_preorder.rs
@@ -1,12 +1,7 @@
-/*
-#23
-Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary
-tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.
+// #23
 
-Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
-Output: [3,9,20,null,null,15,7]
-
-*/
+use std::cell::RefCell;
+use std::rc::Rc;
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct TreeNode {
@@ -26,34 +21,54 @@ impl TreeNode {
         }
     }
 }
-use std::cell::RefCell;
-use std::rc::Rc;
 
-// Depth First Search
-#[allow(dead_code)]
-pub fn build_tree(preorder: Vec<i32>, inorder: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {
-    helper(&preorder, &inorder);
+struct Solution;
 
-    None
-}
+impl Solution {
+    // Depth First Search
+    #[allow(dead_code)]
+    pub fn build_tree(preorder: Vec<i32>, inorder: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {
+        Self::helper(&preorder, &inorder)
+    }
 
-fn helper(preorder: &[i32], inorder: &[i32]) -> Option<Rc<RefCell<TreeNode>>> {
-    if let Some(root) = preorder.first() {
-        let pivot_idx = inorder
-            .iter()
-            .enumerate()
-            .find(|(_, v)| v == &root)
-            .unwrap()
-            .0;
-        return Some(Rc::new(RefCell::new(TreeNode {
-            val: *root,
-            left: helper(&preorder[1..(1 + pivot_idx)], &inorder[0..pivot_idx]),
-            right: helper(&preorder[(1 + pivot_idx)..], &inorder[(pivot_idx + 1)..]),
-        })));
-    } else {
-        return None;
+    fn helper(preorder: &[i32], inorder: &[i32]) -> Option<Rc<RefCell<TreeNode>>> {
+        if let Some(root) = preorder.first() {
+            let pivot_idx = inorder
+                .iter()
+                .enumerate()
+                .find(|(_, v)| v == &root)
+                .unwrap()
+                .0;
+            return Some(Rc::new(RefCell::new(TreeNode {
+                val: *root,
+                left: Self::helper(&preorder[1..(1 + pivot_idx)], &inorder[0..pivot_idx]),
+                right: Self::helper(&preorder[(1 + pivot_idx)..], &inorder[(pivot_idx + 1)..]),
+            })));
+        } else {
+            return None;
+        }
+    }
+
+    pub fn build_tree_01(preorder: Vec<i32>, inorder: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {
+        Self::helper_01(&preorder, &inorder)
+    }
+
+    fn helper_01(preorder: &[i32], inorder: &[i32]) -> Option<Rc<RefCell<TreeNode>>> {
+        if let Some(first) = preorder.first() {
+            let pivot_idx = inorder
+                .iter()
+                .enumerate()
+                .find(|(_, val)| val == &first)
+                .unwrap()
+                .0;
+
+            return Some(Rc::new(RefCell::new(TreeNode {
+                val: *first,
+                left: Self::helper_01(&preorder[1..=pivot_idx], &inorder[0..pivot_idx]),
+                right: Self::helper_01(&preorder[(pivot_idx + 1)..], &inorder[(pivot_idx + 1)..]),
+            })));
+        } else {
+            return None;
+        }
     }
 }
-
-// Preorder first value is the root.
-//
