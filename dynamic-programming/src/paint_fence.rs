fd2046e2b83d968cb40308fa3be23d7737da8841
Aoi <ht.so.ik0102@gmail.com>
2022-05-06 16:00:30 +0700

    #24

--- dynamic-programming/src/paint_fence.rs
+++ dynamic-programming/src/paint_fence.rs
@@ -1,31 +1,9 @@
-/*
-#24
-There is a fence with n posts, each post can be painted with one of the k colors.
-
-You have to paint all the posts such that no more than two adjacent fence posts have the same color.
-
-Return the total number of ways you can paint the fence.
-
-Note:
-n and k are non-negative integers.
-
-Input: n = 3, k = 2
-Output: 6
-
-           post1  post2  post3
- -----      -----  -----  -----
-   1         c1     c1     c2
-   2         c1     c2     c1
-   3         c1     c2     c2
-   4         c2     c1     c1
-   5         c2     c1     c2
-   6         c2     c2     c1
-
-*/
+// #24
 
 struct Solution;
 
 impl Solution {
+    #[allow(dead_code)]
     pub fn num_ways(n: i32, k: i32) -> i32 {
         if n == 0 || k == 0 {
             return 0;
@@ -60,6 +38,5 @@ mod tests {
     fn test_num_ways() {
         let result = Solution::num_ways(1, 2);
         println!("Result: {:?}", result);
-        // assert_eq!(result, 6);
     }
 }
