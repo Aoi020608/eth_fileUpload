bcc10fcfae8a77ebae38f10953edefac6a817819
Aoi <ht.so.ik0102@gmail.com>
2022-05-06 11:18:20 +0700

    #16

--- tree/src/minimum_depth_of_binary_tree.rs
+++ tree/src/minimum_depth_of_binary_tree.rs
@@ -1,10 +1,4 @@
-/*
-#16
-Given a binary tree, find its minimum depth.
-The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
-
-
-*/
+// #16
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct TreeNode {
@@ -15,6 +9,7 @@ pub struct TreeNode {
 
 impl TreeNode {
     #[inline]
+    #[allow(dead_code)]
     pub fn new(val: i32) -> Self {
         TreeNode {
             val,
@@ -27,77 +22,115 @@ use std::cell::RefCell;
 use std::cmp;
 use std::rc::Rc;
 
-// BFS
-pub fn min_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
-    if root.is_none() {
-        return 0;
-    }
+struct Solution;
 
-    let mut depth = 1;
-    let mut q = vec![root.clone()];
+impl Solution {
+    // BFS
+    #[allow(dead_code)]
+    pub fn min_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        if root.is_none() {
+            return 0;
+        }
 
-    while q.len() > 0 {
-        let mut new_q = vec![];
+        let mut depth = 1;
+        let mut q = vec![root.clone()];
 
-        for opt in q.iter() {
-            if let Some(node) = opt {
-                let bor_node = node.borrow();
-                let mut left_none = false;
-                let mut right_none = false;
+        while q.len() > 0 {
+            let mut new_q = vec![];
 
-                match &bor_node.left {
-                    None => left_none = true,
-                    Some(left) => {
-                        new_q.push(bor_node.left.clone());
+            for opt in q.iter() {
+                if let Some(node) = opt {
+                    let bor_node = node.borrow();
+                    let mut left_none = false;
+                    let mut right_none = false;
+
+                    match &bor_node.left {
+                        None => left_none = true,
+                        Some(_) => {
+                            new_q.push(bor_node.left.clone());
+                        }
                     }
-                }
 
-                match &bor_node.right {
-                    None => right_none = true,
-                    Some(right) => {
-                        new_q.push(bor_node.right.clone());
+                    match &bor_node.right {
+                        None => right_none = true,
+                        Some(_) => {
+                            new_q.push(bor_node.right.clone());
+                        }
                     }
-                }
 
-                if left_none && right_none {
-                    return depth;
+                    if left_none && right_none {
+                        return depth;
+                    }
                 }
             }
+
+            if new_q.len() > 0 {
+                depth += 1;
+            }
+            q = new_q;
+        }
+        depth
+    }
+
+    // DFS
+    #[allow(dead_code)]
+    pub fn min_depth_1(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        let mut res: i32 = i32::max_value();
+
+        let mut depth = 1;
+
+        match root {
+            Some(node) => {
+                Self::get_min_depth(node, &mut depth, &mut res);
+                res
+            }
+            None => 0,
+        }
+    }
+
+    fn get_min_depth(node: Rc<RefCell<TreeNode>>, depth: &mut i32, result: &mut i32) {
+        if node.borrow().left.is_none() && node.borrow().right.is_none() {
+            *result = cmp::min(*result, *depth);
         }
 
-        if new_q.len() > 0 {
-            depth += 1;
+        if let Some(left) = node.borrow().left.clone() {
+            Self::get_min_depth(left, depth, result);
         }
-        q = new_q;
-    }
-    depth
-}
 
-// DFS
-pub fn min_depth_1(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
-    let mut res: i32 = i32::max_value();
-
-    let mut depth = 1;
-
-    match root {
-        Some(node) => {
-            get_min_depth(node, &mut depth, &mut res);
-            res
+        if let Some(right) = node.borrow().right.clone() {
+            Self::get_min_depth(right, depth, result);
         }
-        None => 0,
-    }
-}
-
-fn get_min_depth(node: Rc<RefCell<TreeNode>>, depth: &mut i32, result: &mut i32) {
-    if node.borrow().left.is_none() && node.borrow().right.is_none() {
-        *result = cmp::min(*result, *depth);
     }
 
-    if let Some(left) = node.borrow().left.clone() {
-        get_min_depth(left, depth, result);
+    #[allow(dead_code)]
+    pub fn min_depth_02(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        let mut min_dep = i32::max_value();
+
+        match root {
+            None => 0,
+            Some(node) => {
+                // let mut bor_node = node.borrow();
+                let mut depth = 1;
+                Self::dfs(node, &mut depth, &mut min_dep);
+
+                min_dep
+            }
+        }
     }
 
-    if let Some(right) = node.borrow().right.clone() {
-        get_min_depth(right, depth, result);
+    fn dfs(node: Rc<RefCell<TreeNode>>, depth: &mut i32, min_dep: &mut i32) {
+        let bor_node = node.borrow();
+
+        if bor_node.left.is_none() && bor_node.right.is_none() {
+            *min_dep = cmp::min(*min_dep, *depth);
+        }
+
+        if bor_node.left.is_some() {
+            Self::dfs(bor_node.left.clone().unwrap(), &mut (*depth + 1), min_dep);
+        }
+
+        if bor_node.right.is_some() {
+            Self::dfs(bor_node.right.clone().unwrap(), &mut (*depth + 1), min_dep);
+        }
     }
 }
