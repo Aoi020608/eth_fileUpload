504120254e797f13e1b1c48d0ebe4c72308c58c8
Aoi <ht.so.ik0102@gmail.com>
2022-05-10 09:45:02 +0700

    Add new implementation of list_2

--- linkedlist/src/remove_duplicates_from_sorted_list_2.rs
+++ linkedlist/src/remove_duplicates_from_sorted_list_2.rs
@@ -105,6 +105,38 @@ impl Solution {
             }
         }
     }
+
+    #[allow(dead_code)]
+    pub fn delete_duplicates_03(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut to_remove = std::i32::MIN;
+        let mut dummy = Some(Box::new(ListNode {
+            val: to_remove,
+            next: head,
+        }));
+        let mut ptr = dummy.as_mut();
+        while let Some(node) = ptr {
+            match (
+                node.next.as_ref().and_then(|n| Some(n.val)),
+                node.next
+                    .as_ref()
+                    .and_then(|n| n.next.as_ref().and_then(|m| Some(m.val))),
+            ) {
+                (Some(a), _) if a == to_remove => {
+                    node.next = node.next.as_mut().unwrap().next.take();
+                    ptr = Some(node);
+                }
+                (Some(a), Some(b)) if a == b => {
+                    to_remove = a;
+                    ptr = Some(node);
+                }
+                _ => {
+                    ptr = node.next.as_mut();
+                }
+            }
+        }
+
+        dummy.unwrap().next.take()
+    }
 }
 
 #[cfg(test)]
@@ -132,7 +164,7 @@ mod tests {
             })),
         }));
 
-        let sorted = Solution::delete_duplicates_02(head);
+        let sorted = Solution::delete_duplicates_03(head);
         println!("{:?}", sorted);
     }
 }
