ab1dce49598ab6a662e5cb44e3e6e835875a59e3
Aoi1011 <ht.so.ik0102@gmail.com>
2022-10-08 22:00:22 +0700

    Update

--- array-and-hashing/src/contains_duplicate.rs
+++ array-and-hashing/src/contains_duplicate.rs
@@ -1,7 +1,19 @@
-pub fn contains_duplicate(nums: Vec<i32>) -> bool {
-    println!("Hello, world!");
+use std::collections::HashMap;
 
-    true
+#[allow(dead_code)]
+pub fn contains_duplicate(nums: Vec<i32>) -> bool {
+    let contain = false;
+
+    let mut map = HashMap::new();
+
+    for num in nums {
+        if map.contains_key(&num) {
+            return true;
+        }
+        map.insert(num, true);
+    }
+
+    contain
 }
 
 #[cfg(test)]
@@ -9,5 +21,14 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_contains_duplicate() {}
+    fn test_contains_duplicate() {
+        let nums = vec![1, 2, 3, 1];
+        assert!(contains_duplicate(nums));
+    }
+
+    #[test]
+    fn test_does_not_contains_duplicate() {
+        let nums = vec![1, 2, 3, 4];
+        assert!(!contains_duplicate(nums));
+    }
 }
