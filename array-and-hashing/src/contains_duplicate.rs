51da5e0b3e135fa703cbe1a84d3d827cad2a79f5
Aoi1011 <ht.so.ik0102@gmail.com>
2022-10-08 22:08:06 +0700

    Use HashSet

--- array-and-hashing/src/contains_duplicate.rs
+++ array-and-hashing/src/contains_duplicate.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 #[allow(dead_code)]
 pub fn contains_duplicate(nums: Vec<i32>) -> bool {
@@ -16,6 +16,21 @@ pub fn contains_duplicate(nums: Vec<i32>) -> bool {
     contain
 }
 
+#[allow(dead_code)]
+pub fn contains_duplicate_1(nums: Vec<i32>) -> bool {
+    let mut set = HashSet::new();
+
+    for num in nums {
+        if set.contains(&num) {
+            return true;
+        }
+
+        set.insert(num);
+    }
+
+    false
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -23,18 +38,18 @@ mod tests {
     #[test]
     fn test_contains_duplicate() {
         let nums = vec![1, 2, 3, 1];
-        assert!(contains_duplicate(nums));
+        assert!(contains_duplicate_1(nums));
     }
 
     #[test]
     fn test_does_not_contains_duplicate() {
         let nums = vec![1, 2, 3, 4];
-        assert!(!contains_duplicate(nums));
+        assert!(!contains_duplicate_1(nums));
     }
 
     #[test]
     fn test_many_number_test_case() {
         let nums = vec![1, 1, 1, 3, 3, 4, 3, 2, 4, 2];
-        assert!(contains_duplicate(nums));
+        assert!(contains_duplicate_1(nums));
     }
 }
