5a62918557232adcf3c59828a06184f5dde1e678
Aoi1011 <ht.so.ik0102@gmail.com>
2022-11-15 20:43:02 +0700

    Create testcase

--- array-and-hashing/src/two_sum.rs
+++ array-and-hashing/src/two_sum.rs
@@ -1,24 +1,30 @@
 pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-  let ret_vec = Vec::new(); 
-
-    
-
+    let mut ret_vec = Vec::new();
     let mut nums_len = nums.len();
 
-    for num in nums {
-        ret_vec.push(num)
-    }
-
-    for i in nums.len() {
-        for j in nums_len() {
-           if nums[i] == (target - nums[j]) {
-                
-           } 
-        } 
+    for i in 0..nums.len() {
+        for j in 0..nums_len {
+            if nums[i] == (target - nums[j]) {
+                ret_vec.push(nums[i]);
+                ret_vec.push(nums[j]);
+            }
+        }
 
         nums_len -= 1;
     }
 
     ret_vec
-    
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_two_sum() {
+        let inputs = vec![2, 7, 11, 15];
+        let target = 9;
+
+        assert_eq!(two_sum(inputs, target), [0, 1]);
+    }
 }
