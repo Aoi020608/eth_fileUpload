1c489812390a4fa928ae3820af429e403116257f
Aoi1011 <ht.so.ik0102@gmail.com>
2022-11-20 23:19:54 +0700

    Create test case for group_anagrams function

--- array-and-hashing/src/group_anagrams.rs
+++ array-and-hashing/src/group_anagrams.rs
@@ -1,7 +1,28 @@
-
 pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
     let mut ret_vec = Vec::new();
     ret_vec.push(vec!["".to_string()]);
 
     ret_vec
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_group_anagrams() {
+        let inputs = vec![
+            "eat".to_string(),
+            "tea".to_string(),
+            "tan".to_string(),
+            "ate".to_string(),
+            "nat".to_string(),
+            "bat".to_string(),
+        ];
+
+        assert_eq!(
+            group_anagrams(inputs),
+            [vec!["bat"], vec!["nat", "tan"], vec!["ate", "eat", "tea"]]
+        );
+    }
 }
