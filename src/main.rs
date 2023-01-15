6c312e8732faa91ba245cabd0ddf78b5a0a8c9cc
Aoi1011 <ht.so.ik0102@gmail.com>
2023-01-15 22:27:45 +0700

    Declare struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use byteorder::{LittleEndian, BigEndian};
+
 fn main() {
     println!("Hello, world!");
 }
+
+struct Version {
+   version: LittleEndian,
+    services: LittleEndian,
+    timestamp: LittleEndian,
+    addr_recv: NetAddr,
+    addr_from: NetAddr,
+    nonce: LittleEndian,
+    user_agent: LittleEndian,
+    start_height: LittleEndian,
+    relay: bool,
+}
+
+struct MessageHeader {
+    magic: LittleEndian,
+    command_name: String,
+    payload_size: LittleEndian,
+    check_sum: String,
+} 
+
+struct NetAddr {
+    services: LittleEndian,
+    ip: String,
+    port: BigEndian,
+}
+
+struct Message {
+    command_name: String,
+}
+
+impl Message {
+    
+}
+
