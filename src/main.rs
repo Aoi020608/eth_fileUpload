e48c3292c5b6fe67cd41ebb8d585fbd9536ea9f3
Aoi1011 <ht.so.ik0102@gmail.com>
2023-01-16 16:49:24 +0700

    Update Types

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
+use std::borrow::Cow;
+
 use byteorder::{LittleEndian, BigEndian};
 
 fn main() {
     println!("Hello, world!");
 }
 
-struct Version {
+trait Version {
+    fn is_verack(&self) -> bool;
+    fn is_version(&self) -> Option<VersionMessage>;
+}
+
+enum NetworkMessage {
+    Version(VersionMessage),
+    Verack,
+}
+
+struct VersionMessage {
    version: LittleEndian,
     services: LittleEndian,
     timestamp: LittleEndian,
-    addr_recv: NetAddr,
-    addr_from: NetAddr,
+    addr_recv: Address,
+    addr_from: Address,
     nonce: LittleEndian,
     user_agent: LittleEndian,
     start_height: LittleEndian,
@@ -17,17 +29,22 @@ struct Version {
 }
 
 struct MessageHeader {
-    magic: LittleEndian,
-    command_name: String,
-    payload_size: LittleEndian,
-    check_sum: String,
+    magic: u32,
+    command: CommandString,
+    length: u32,
+    checksum: u32,
+    payload: Vec<u8>,
 } 
 
-struct NetAddr {
-    services: LittleEndian,
-    ip: String,
-    port: BigEndian,
+struct Address {
+    services: ServiceFlags,
+    address: [u16; 8],
+    port: u16,
 }
+
+struct ServiceFlags(u64);
+
+struct CommandString(Cow<'static, str>);
 
 struct Message {
     command_name: String,
