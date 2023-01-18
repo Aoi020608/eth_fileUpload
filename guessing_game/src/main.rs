ed15fcdc9fec125e2b9091285ad1b9f4d09e0b60
Aoi1011 <ht.so.ik0102@gmail.com>
2023-01-18 14:50:42 +0700

    Fmt

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -10,28 +10,28 @@ fn main() {
     println!("The secret number is: {secret_number}");
 
     loop {
-    println!("Please input your guess.");
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num, 
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
             Err(_) => continue,
-    };
+        };
 
-    println!("You guessed: {guess}");
+        println!("You guessed: {guess}");
 
-    match guess.cmp(&secret_number) {
-        std::cmp::Ordering::Less => println!("Too small!"),
-        std::cmp::Ordering::Greater => println!("Too big!"),
-        std::cmp::Ordering::Equal => {
+        match guess.cmp(&secret_number) {
+            std::cmp::Ordering::Less => println!("Too small!"),
+            std::cmp::Ordering::Greater => println!("Too big!"),
+            std::cmp::Ordering::Equal => {
                 println!("You win!");
                 break;
+            }
         }
-    }
     }
 }
