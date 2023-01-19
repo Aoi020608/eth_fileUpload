43a2e1e23e0be53720bcf4fa8c22c718af6ab4ad
Aoi1011 <ht.so.ik0102@gmail.com>
2023-01-19 12:58:28 +0700

    Lifetime

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,26 +1,44 @@
 use std::collections::HashMap;
 
-struct Point<T> {
-    x: T,
-    y: T,
+pub trait Summary {
+    fn summary(&self) -> String;
 }
 
-impl<T> Point<T> {
-    fn x(&self) -> &T {
-        &self.x
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summary(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summary(&self) -> String {
+        format!("{}: {}", self.username, self.content)
     }
 }
 
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
+    let news_article = Tweet {
+        username: String::from("hello"),
+        content: String::from("of course"),
+        reply: false,
+        retweet: false,
+    };
 
-    let res = largest(&number_list);
-    println!("The largest number is {res}");
-
-    let char_list = vec!['y', 'm', 'a', 'q'];
-
-    let res = largest(&char_list);
-    println!("The largest char is {res}");
+    println!("1 new tweet: {}", news_article.summary());
 }
 
 fn largest<T: PartialOrd>(list: &[T]) -> &T {
