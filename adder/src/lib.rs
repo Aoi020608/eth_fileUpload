6a27c9f1e600fe5f424216dc1e471f0feaa452e0
Aoi1011 <ht.so.ik0102@gmail.com>
2023-01-21 08:52:53 +0700

    Update

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,44 +1,77 @@
-pub fn greeting(name: &str) -> String {
-    format!("Hello: {}!", name)
+#[derive(Debug, PartialEq, Copy, Clone)]
+pub enum ShirtColor {
+    Red,
+    Blue,
 }
 
-#[derive(Debug)]
-pub struct Rectangle {
-    pub width: u32,
-    pub height: u32,
+pub struct Inventry {
+    pub shirts: Vec<ShirtColor>,
 }
 
-impl Rectangle {
-    pub fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
+impl Inventry {
+    pub fn giveway(&self, user_preference: Option<ShirtColor>) -> ShirtColor {
+        user_preference.unwrap_or_else(|| self.most_stocked())
+        
     }
-}
 
-pub fn prints_and_returns_10(a: i32) -> i32 {
-    println!("I got the value {a}");
-    10
-}
+    pub fn most_stocked(&self) -> ShirtColor {
+let mut num_red = 0;
+        let mut num_blue = 0;
 
-pub fn add_two(a: i32) -> i32 {
-    a + 2
+        for color in &self.shirts {
+            match color {
+                ShirtColor::Red => num_red += 1,
+                ShirtColor::Blue => num_blue += 1,
+            }
+        }
+
+        if num_red > num_blue {
+            ShirtColor::Red
+        } else {
+            ShirtColor::Blue
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::thread;
+
     use super::*;
 
     #[test]
-    fn add_two_and_two() {
-        assert_eq!(4, add_two(2));
+    fn test_shirt_color() {
+        let store = Inventry {
+            shirts: vec![ShirtColor::Blue, ShirtColor::Blue, ShirtColor::Red]
+        };
+
+        let user_pref1 = Some(ShirtColor::Red);
+        let give_way1 = store.giveway(user_pref1);
+        println!("The user with preference {:?} gets {:?}", user_pref1, give_way1);
+
+        let user_pref2 = Some(ShirtColor::Blue);
+        let give_way2 = store.giveway(user_pref2);
+        println!("The user with preference {:?} gets {:?}", user_pref2, give_way2);
     }
 
     #[test]
-    fn add_three_and_two() {
-        assert_eq!(5, add_two(3));
+    fn test_capturing_reference() {
+        let mut list = vec![1, 2, 3];
+        println!("Before defining closure: {:?}", list);
+
+        let mut only_borrows = || list.push(1);
+
+        only_borrows();
+        println!("After calling closure: {:?}", list);
+
     }
 
     #[test]
-    fn one_hundred() {
-        assert_eq!(102, add_two(100));
+    fn test_thread() {
+        let list = vec![1, 2, 3];
+        println!("Before defining closure: {:?}", list);
+
+        thread::spawn(move || println!("From thread: {:?}", list)).join().unwrap();
+
     }
 }
